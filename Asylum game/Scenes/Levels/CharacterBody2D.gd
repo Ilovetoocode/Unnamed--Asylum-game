005e4233284808a22584f82272557f5fd122ffa7
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2023-10-13 14:02:14 -0600

    HE WALKS

--- Asylum game/Scenes/Levels/CharacterBody2D.gd
+++ Asylum game/Scenes/Levels/CharacterBody2D.gd
@@ -8,7 +8,12 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var _animated_sprite = $AnimatedSprite2D
 func _process(_delta):
-	_animated_sprite.play("idle")
+	if Input.is_action_pressed("Left"):
+		_animated_sprite.play("Walkleft")
+	elif Input.is_action_pressed("right"):
+		_animated_sprite.play("walkright")
+	else:
+		_animated_sprite.play("idle")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
