d8addded9e94c7aaeadae59ce663f215e792dc68
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2023-10-14 08:38:14 -0600

    Finishing up the inverted world base
    Waiting on the document stuff and some other things, and that's about it.

---
+++ Asylum game/Scenes/Levels/Inv.gd
@@ -0,0 +1,23 @@
+extends Area2D
+var collide=false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+		if body.is_in_group("Player"):
+			collide=true
+func _input(event):
+	if  event.is_action_pressed("up") and collide:
+		collide=false
+		get_tree().change_scene_to_file("res://Scenes/Levels/Stranger things reference/Invrm2.tscn")
+
+func _on_body_exited(body):
+	collide=false
