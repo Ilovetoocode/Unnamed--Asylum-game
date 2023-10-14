f68d662f9bc1a668dbd42c57d6d1e67cd027ef7a
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2023-10-14 12:53:56 -0600

    Last of the cleanup for  now

--- Asylum game/Scenes/Levels/Inv3.gd
+++ Asylum game/Scenes/Levels/Inv3.gd
@@ -1,16 +1,5 @@
 extends Area2D
 var collide=false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 		if body.is_in_group("Player"):
 			collide=true
@@ -19,5 +8,5 @@ func _input(event):
 		collide=false
 		get_tree().change_scene_to_file("res://Scenes/Levels/Stranger things reference/Invrm4.tscn")
 
-func _on_body_exited(body):
+func _on_body_exited(_body):
 	collide=false
